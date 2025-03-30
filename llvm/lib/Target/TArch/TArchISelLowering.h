53df926b154d77d50a52a66e08ba6725853737db
Denis Boborukhin <denis.boboruchin@gmail.com>
2025-03-30 15:27:07 +0300

    [TArch] feat: add Calling Convention, MachineFunction and Lowering Functions

---
+++ llvm/lib/Target/TArch/TArchISelLowering.h
@@ -0,0 +1,70 @@
+#ifndef LLVM_LIB_TARGET_TARCH_TARCHISELLOWERING_H
+#define LLVM_LIB_TARGET_TARCH_TARCHISELLOWERING_H
+
+#include "TArch.h"
+#include "llvm/CodeGen/SelectionDAG.h"
+#include "llvm/CodeGen/TargetLowering.h"
+
+namespace llvm {
+
+class TArchSubtarget;
+class TArchTargetMachine;
+
+namespace TArchISD {
+
+enum NodeType : unsigned {
+  // Start the numbering where the builtin ops and target ops leave off.
+  FIRST_NUMBER = ISD::BUILTIN_OP_END,
+  RET,
+  CALL,
+  BR_CC,
+};
+
+} // namespace TArchISD
+
+class TArchTargetLowering : public TargetLowering {
+public:
+  explicit TArchTargetLowering(const TargetMachine &TM, const TArchSubtarget &STI);
+
+  /// This method returns the name of a target specific DAG node.
+  const char *getTargetNodeName(unsigned Opcode) const override;
+
+  /// Return true if the addressing mode represented by AM is legal for this
+  /// target, for a load/store of the specified type.
+  bool isLegalAddressingMode(const DataLayout &DL, const AddrMode &AM, Type *Ty,
+                             unsigned AS,
+                             Instruction *I = nullptr) const override;
+
+  TArchSubtarget const &getSubtarget() const { return STI; }
+
+private:
+  const TArchSubtarget &STI;
+
+  void ReplaceNodeResults(SDNode *N, SmallVectorImpl<SDValue> &Results,
+                          SelectionDAG &DAG) const override;
+
+  SDValue PerformDAGCombine(SDNode *N, DAGCombinerInfo &DCI) const override;
+
+  SDValue LowerFormalArguments(SDValue Chain, CallingConv::ID CallConv,
+                               bool IsVarArg,
+                               const SmallVectorImpl<ISD::InputArg> &Ins,
+                               const SDLoc &DL, SelectionDAG &DAG,
+                               SmallVectorImpl<SDValue> &InVals) const override;
+
+  SDValue LowerCall(TargetLowering::CallLoweringInfo &CLI,
+                    SmallVectorImpl<SDValue> &InVals) const override;
+
+  SDValue LowerReturn(SDValue Chain, CallingConv::ID CallConv, bool IsVarArg,
+                      const SmallVectorImpl<ISD::OutputArg> &Outs,
+                      const SmallVectorImpl<SDValue> &OutVals, const SDLoc &DL,
+                      SelectionDAG &DAG) const override;
+
+  bool CanLowerReturn(CallingConv::ID CallConv, MachineFunction &MF,
+                      bool IsVarArg,
+                      const SmallVectorImpl<ISD::OutputArg> &ArgsFlags,
+                      LLVMContext &Context, const Type *RetTy) const override;
+};
+
+} // end namespace llvm
+
+#endif // LLVM_LIB_TARGET_TARCH_TARCHISELLOWERING_H
